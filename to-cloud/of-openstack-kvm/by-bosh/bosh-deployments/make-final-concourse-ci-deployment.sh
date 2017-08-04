3d3b63493c50e92308a3cb9b2eb07d8f150d70d5
Jim Lin <jimlintw922@gmail.com>
2017-08-04 16:29:48 +0800

    update make-final-concourse-ci-deployment.sh

---
+++ to-cloud/of-openstack-kvm/by-bosh/bosh-deployments/make-final-concourse-ci-deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+<<PSEUDO
+bosh-cli --ca-cert certs/rootCA.pem -e $BOSH_DIRECTOR_IP login
+
+spiff merge cloud-config-template.yml openstack-stub.yml > cloud-config.yml
+
+bosh-cli --ca-cert certs/rootCA.pem -e $BOSH_DIRECTOR_IP update-cloud-config cloud-config.yml
+
+bosh-cli --ca-cert certs/rootCA.pem -e $BOSH_DIRECTOR_IP upload-stemcell STEMCELL-URL
+
+bosh-cli --ca-cert certs/rootCA.pem -e $BOSH_DIRECTOR_IP env # to get director uuid
+
+MODIFY director-stub.yml and concourse-stub.yml
+
+spiff merge concourse-template.yml openstack-stub.yml director-stub.yml concourse-stub.yml > concourse-deployment.yml
+PSEUDO
