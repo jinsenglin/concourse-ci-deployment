03ef58ddb5739c4516dfb041e1c254636264b1ad
Jim Lin <jimlintw922@gmail.com>
2017-08-04 14:36:36 +0800

    update of-openstack-kvm

---
+++ to-cloud/of-openstack-kvm/by-bosh/bosh-deployments/make-final-bosh-director-deployment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [ -f rc ]; then
+    source rc
+
+    sed "s/REPLACE_WITH_OS_AUTH_URL/$OS_AUTH_URL/g" templates/openstack-stub.yml | \
+    sed "s/REPLACE_WITH_OS_PROJECT/$OS_PROJECT/g" | \
+    sed "s/REPLACE_WITH_OS_DOMAIN/$OS_DOMAIN/g" | \
+    sed "s/REPLACE_WITH_OS_USERNAME/$OS_USERNAME/g" | \
+    sed "s/REPLACE_WITH_OS_PASSWORD/$OS_PASSWORD/g" | \
+    sed "s/REPLACE_WITH_OS_RES_SECURITY_GROUP/$OS_RES_SECURITY_GROUP/g" | \
+    sed "s/REPLACE_WITH_OS_RES_KEY_PAIR/$OS_RES_KEY_PAIR/g" | \
+    sed "s/REPLACE_WITH_OS_RES_NETWORK_SUBNET_CIDR/$OS_RES_NETWORK_SUBNET_CIDR/g" | \
+    sed "s/REPLACE_WITH_OS_RES_NETWORK_SUBNET_GW/$OS_RES_NETWORK_SUBNET_GW/g" | \
+    sed "s/REPLACE_WITH_OS_RES_NETWORK_UUID/$OS_RES_NETWORK_UUID/g" | \
+    sed "s/REPLACE_WITH_OS_RES_NETWORK_SUBNET_DHCP/$OS_RES_NETWORK_SUBNET_DHCP/g" | \
+    sed "s/REPLACE_WITH_OS_RES_NETWORK_SUBNET_DIRECTOR/$OS_RES_NETWORK_SUBNET_DIRECTOR/g" | \
+    sed "s/REPLACE_WITH_OS_RES_FLOATING_IP_BOSH_DIRECTOR/$OS_RES_FLOATING_IP_BOSH_DIRECTOR/g" | \
+    tee deployments/openstack-stub.yml
+else
+    echo Skipping making deployments/openstack-stub.yml
+fi
+
+
