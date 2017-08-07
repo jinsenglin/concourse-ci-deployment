9e7ddeecce13bdfb52246db0d85e2292baaeae6d
Jim Lin <jimlintw922@gmail.com>
2017-08-07 14:35:13 +0800

    update roles/db/files/installer.sh

--- to-machine/of-vagrant/by-ansible/roles/db/files/installer.sh
+++ to-machine/of-vagrant/by-ansible/roles/db/files/installer.sh
@@ -3,7 +3,14 @@
 echo "Current user: $(whoami)"
 
 apt-get update
-apt-get install -y postgresql postgresql-contrib
+#apt-get remove -y postgresql
+apt-get install -y postgresql=9.5+173 postgresql-contrib=9.5+173
 
 sudo -u postgres createuser concourse
 sudo -u postgres createdb --owner=concourse atc
+
+# change 127.0.0.1:5432 to 0.0.0.0:5432
+systemctl stop postgresql
+sed -i "$ a host    all             all             0.0.0.0/0                trust" /etc/postgresql/9.5/main/pg_hba.conf
+sed -i "/listen_addresses/ a listen_addresses = '*'"                                /etc/postgresql/9.5/main/postgresql.conf
+systemctl start postgresql
