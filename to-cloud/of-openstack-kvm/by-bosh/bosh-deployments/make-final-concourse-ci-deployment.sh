a0f7bd4e231eadf3a091dcb69c7de86e22f3bfcc
Jim Lin <jimlintw922@gmail.com>
2017-08-07 11:03:44 +0800

    update of-openstack-kvm

--- to-cloud/of-openstack-kvm/by-bosh/bosh-deployments/make-final-concourse-ci-deployment.sh
+++ to-cloud/of-openstack-kvm/by-bosh/bosh-deployments/make-final-concourse-ci-deployment.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 <<PSEUDO
-MODIFY concourse-stub.yml
-
 spiff merge concourse-template.yml openstack-stub.yml director-stub.yml concourse-stub.yml > concourse-deployment.yml
 PSEUDO
+
+set -e
+
+[ -f rc ] && source rc
+
+if [ -z $CONCOURSE_CI_HOST ]; then
+    if [ -z $OS_RES_FLOATING_IP_CONCOURSE_CI ]; then
+        echo "Error. Missing environment variable CONCOURSE_CI_HOST."
+        exit 1
+    else
+        CCIP=$OS_RES_FLOATING_IP_CONCOURSE_CI
+    fi
+else
+    CCIP=$CONCOURSE_CI_HOST
+fi
+
+########################
+
+sed "s/REPLACE_WITH_OS_RES_FLOATING_IP_CONCOURSE_CI/$CCIP/g" templates/concourse-stub.yml | tee deployments/concourse-stub.yml
+
+spiff merge templates/concourse-ci.yml deployments/openstack-stub.yml director-stub.yml deployments/concourse-stub.yml | tee deployments/concourse-ci.yml
