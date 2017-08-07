b0bc4419e57bbd08003713d00ce8535fefcced1c
Jim Lin <jimlintw922@gmail.com>
2017-08-07 16:27:07 +0800

    working on web/files/installer.sh

--- to-machine/of-vagrant/by-ansible/roles/web/files/installer.sh
+++ to-machine/of-vagrant/by-ansible/roles/web/files/installer.sh
@@ -3,13 +3,21 @@
 echo "Current user: $(whoami)"
 
 apt-get update
-wget https://github.com/concourse/concourse/releases/download/v3.3.4/concourse_linux_amd64 -O concourse && chmod +x concourse && mv concourse /usr/local/bin/concourse
+wget -q https://github.com/concourse/concourse/releases/download/v3.3.4/concourse_linux_amd64 -O concourse && chmod +x concourse && mv concourse /usr/local/bin/concourse
 
 mkdir /etc/concourse
-cp $(dirname $0)/service-keys/* /etc/concourse/
+cp $(dirname $0)/concourse-web.service /etc/concourse/concourse-web.service
+cp $(dirname $0)/tsa_host_key.pem /etc/concourse/tsa_host_key.pem
+cp $(dirname $0)/session_signing_key.pem /etc/concourse/session_signing_key.pem
+cp $(dirname $0)/authorized_worker_keys /etc/concourse/authorized_worker_keys
+cp $(dirname $0)/web_environment /etc/concourse/web_environment
+
+ln -sf /etc/concourse/concourse-web.service /etc/systemd/system/concourse-web.service
 
 adduser --system --group concourse
 chown -R concourse:concourse /etc/concourse
+
+#systemctl start concourse-web
 
 #
 touch $(dirname $0)/installed
--- to-machine/of-vagrant/by-ansible/roles/worker/files/installer.sh
+++ to-machine/of-vagrant/by-ansible/roles/worker/files/installer.sh
@@ -3,6 +3,7 @@
 echo "Current user: $(whoami)"
 
 apt-get update
+wget -q https://github.com/concourse/concourse/releases/download/v3.3.4/concourse_linux_amd64 -O concourse && chmod +x concourse && mv concourse /usr/local/bin/concourse
 
 #
 touch $(dirname $0)/installed
