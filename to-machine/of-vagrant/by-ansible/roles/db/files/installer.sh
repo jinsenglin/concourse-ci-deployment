3e90e34815921a76371f7b4207215934aa2be123
Jim Lin <jimlintw922@gmail.com>
2017-08-07 14:05:53 +0800

    update by-ansible/roles/db/files/installer.sh

--- to-machine/of-vagrant/by-ansible/roles/db/files/installer.sh
+++ to-machine/of-vagrant/by-ansible/roles/db/files/installer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Current user: $(whoami)"
+
+apt-get update
+apt-get install -y postgresql postgresql-contrib
+
+sudo -u postgres createuser concourse
+sudo -u postgres createdb --owner=concourse atc
